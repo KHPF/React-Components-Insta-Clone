0c874c379eb547a2541d92eb4445498985d92b38
Faisal H <50386930+KHPF@users.noreply.github.com>
2020-10-20 04:22:10 -0500

    insta clone

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import Comment from './Comment';
-import './Comments.css';
+import React from "react";
+import Comment from "./Comment";
+import "./Comments.css";
 
-const Comments = props => {
+const Comments = (props) => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
 
   return (
     <div>
+      {comments.map((elem) => {
+        return <Comment key={elem.id} comment={elem} />;
+      })}
       {/* map through the comments prop and render a Comment for every piece of data */}
     </div>
   );
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import Post from './Post';
-import './Posts.css';
+import React from "react";
+import Post from "./Post";
+import "./Posts.css";
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
-
+  console.log(posts);
   return (
-    <div className='posts-container-wrapper'>
+    <div className="posts-container-wrapper">
+      {posts.map((post, index) => {
+        return <Post key={index} post={post} likePost={likePost} />;
+      })}
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
